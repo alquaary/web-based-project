d9bf4b06bcbe81a7a3ecea54a0b694fb86d35112
abdalrhman <alquaary77@gmail.com>
2021-04-01 07:57:51 +0300

    adding login form

--- Apo_Company/Login.aspx.designer.cs
+++ Apo_Company/Login.aspx.designer.cs
@@ -13,23 +13,5 @@ namespace Apo_Company
 
     public partial class Login
     {
-
-        /// <summary>
-        /// TextBox1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox1;
-
-        /// <summary>
-        /// TextBox2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox TextBox2;
     }
 }
